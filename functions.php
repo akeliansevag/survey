c57cb8af58f150f3f459f196a5b23c4241210f8d
akeliansevag <akelian.sevag@gmail.com>
2024-11-18 11:34:45 +0200

    initial

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php
+
+if (! defined('_S_VERSION')) {
+	define('_S_VERSION', '1.0.0');
+}
+
+function survey_setup()
+{
+
+	load_theme_textdomain('survey', get_template_directory() . '/languages');
+
+	add_theme_support('title-tag');
+
+	add_theme_support('post-thumbnails');
+
+	register_nav_menus(
+		array(
+			'menu-1' => esc_html__('Primary', 'survey'),
+		)
+	);
+
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		)
+	);
+}
+add_action('after_setup_theme', 'survey_setup');
+
+function survey_scripts()
+{
+	wp_enqueue_style('survey-style', get_stylesheet_uri(), array(), _S_VERSION);
+	// wp_enqueue_script( 'survey-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+}
+add_action('wp_enqueue_scripts', 'survey_scripts');
