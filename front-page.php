d75b8fc277a796e45663dbf85f516be16b08cd1f
akeliansevag <akelian.sevag@gmail.com>
2024-11-19 14:19:10 +0200

    .

--- front-page.php
+++ front-page.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
-
+<section>
+    <img src="<?= get_template_directory_uri() ?>/assets/img/banner.webp" alt="Banner" />
+</section>
 <div class="container">
     <?php the_content(); ?>
 </div>
