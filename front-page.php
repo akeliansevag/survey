4198a6a86b4f8a58c3a579a88d7049f83b3fc97c
akeliansevag <akelian.sevag@gmail.com>
2024-11-20 10:38:48 +0200

    .

--- front-page.php
+++ front-page.php
@@ -2,6 +2,12 @@
 <section>
     <img src="<?= get_template_directory_uri() ?>/assets/img/banner.webp" alt="Banner" />
 </section>
+<section>
+    <div class="container text-center pt-20 pb-10">
+        <h1 class="text-4xl font-bold mb-2">Post event Survey</h1>
+        <p class="text-xl">Please take a few moments to fill in this survey.</p>
+    </div>
+</section>
 <div class="container">
     <?php the_content(); ?>
 </div>
