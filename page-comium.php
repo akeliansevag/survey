c70e8a5c2d0433ae45b19488fcf25f9dda8ee18c
akeliansevag <akelian.sevag@gmail.com>
2024-12-11 16:35:51 +0200

    .

---
+++ page-comium.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <!-- <link href="<?= get_template_directory_uri() ?>/assets/img/faviconmonty.webp" rel="shortcut icon">
+	<link href="<?= get_template_directory_uri() ?>/assets/img/faviconmonty.webp" rel="apple-touch-icon-precomposed"> -->
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <section>
+        <div class="container text-center pt-20 pb-10">
+            <h1>Comium</h1>
+        </div>
+    </section>
+
+    <?php wp_footer(); ?>
+
+</body>
+
+</html>
